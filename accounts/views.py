ab8e8fb476ae3c7733676589f97483e47dd8a44a
Jose Rodriguez <josergzruiz@outlook.es>
2021-11-10 17:43:34 -0600

    dasboard ya funcionando y mandando correos para los nuevos usuarios

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,12 @@ from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.core.mail import EmailMessage
 
 
 # Create your views here.
@@ -21,8 +27,21 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name,last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
-            messages.success(request,'!Se registro el usuario exitosamente!')
-            return redirect('register')
+
+            current_site = get_current_site(request)
+            mail_subject = 'Por favor activa tu cuenta en M&J | Shop'
+            body = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, body, to=[to_email])
+            send_email.send()
+
+            #messages.success(request,'!Se registro el usuario exitosamente!')
+            return redirect('/accounts/login/?command=verification&email='+email)
             
     context = {
         'form': form,
@@ -38,7 +57,8 @@ def login(request):
 
         if user is not None:
             auth.login(request,user)
-            return redirect('home')
+            messages.success(request, '¡Has iniciado sesion exitosamente!')
+            return redirect('dashboard')
         else:
             messages.error(request, 'Las credenciales son incorrectas')
             return redirect('login')
@@ -51,3 +71,36 @@ def logout(request):
     messages.success(request, 'Has salido de sesion')
 
     return redirect('login')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Felicidades, tu cuenta esta activa!')
+        return redirect('login')
+    else:
+        messages.error(request, 'La activacion es invalida')
+        return redirect('register')
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    '''orders = Order.objects.order_by(
+        '-created_at').filter(user_id=request.user.id, is_ordered=True)
+    orders_count = orders.count()
+
+    userprofile = UserProfile.objects.get(user_id=request.user.id)
+
+    context = {
+        'orders_count': orders_count,
+        'userprofile': userprofile,
+    }'''
+
+    return render(request, 'accounts/dashboard.html')
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,6 +138,12 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'verificacion.correo.mj@gmail.com'
+EMAIL_HOST_PASSWORD = 'JoseRodriguez2021'
+EMAIL_USE_TLS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
