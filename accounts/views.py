0654e2685867dcfc9aa2113d8f9de7292d96c3d1
Jose Rodriguez <josergzruiz@outlook.es>
2021-11-11 10:52:04 -0600

    Si funciono la migracion y mando a la base de datos el producto con el usuario

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,8 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
 from django.core.mail import EmailMessage
 
-
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
 # Create your views here.
 
 def register(request):
@@ -56,6 +57,19 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exist= CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exist:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    for item in cart_item:
+                        item.user = user
+                        item.save()
+            except:
+                pass
+
+
             auth.login(request,user)
             messages.success(request, '¡Has iniciado sesion exitosamente!')
             return redirect('dashboard')
